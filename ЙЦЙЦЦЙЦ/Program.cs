5196d0e3cd90c89f33ff43579219b1adfbda3d00
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2022-04-11 15:39:55 +0300

    Add files via upload

---
+++ ЙЦЙЦЦЙЦ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ЙЦЙЦЦЙЦ
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
